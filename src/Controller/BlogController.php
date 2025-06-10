8515a9bd28f29c2b64962ca5829e00013cbbd4c2
Iloduba <kelis.iloduba@etu.univ-st-etienne.fr>
2025-06-10 06:01:54 +0200

    update

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,20 +8,46 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'app_blog')]
-    public function index(): Response
+    #[Route('/blog/accueil', name: 'app_accueil')]
+    public function accueil() : Response 
+    {   
+        return $this->render('blog/accueil.html.twig', [
+            'controller_name' => 'BlogController',
+            'nom' => 'Iloduba',
+            'prenom' => 'Kelis',
+            'groupe' => 'A2',
+        ]);
+    }
+
+    #[Route('/blog/loisirs', name: 'app_loisirs')]
+    public function loisirs(): Response
     {
-        return $this->render('blog/index.html.twig', [
+        return $this->render('blog/loisirs.html.twig', [
             'controller_name' => 'BlogController',
         ]);
     }
 
-    #[Route('/blog/home', name: 'app_home')]
-    public function home() : Response 
-    {   
-        return $this->render('blog/home/home.html.twig', [
+    #[Route('/blog/portfolio', name: 'app_portfolio')]
+    public function portfolio(): Response
+    {
+        return $this->render('blog/portfolio.html.twig', [
             'controller_name' => 'BlogController',
-            'title' => 'Bienvenue',
+        ]);
+    }
+
+    #[Route('/blog/cv', name: 'app_cv')]
+    public function cv(): Response
+    {
+        return $this->render('blog/cv.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+
+    #[Route('/blog/cyber', name: 'app_cyber')]
+    public function cyber(): Response
+    {
+        return $this->render('blog/cyber.html.twig', [
+            'controller_name' => 'BlogController',
         ]);
     }
 }
